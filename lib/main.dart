74345edee6029aecca9187ee69fb24e2887897f5
jeferson rocha <jeferson.rocha8@gmail.com>
2019-11-20 11:14:43 -0300

    add titulo do projeto

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:minhas_viagens/Home.dart';
 
 void main() => runApp(MaterialApp(
+  title: "Minhas viagens",
   home: Home(),
+  debugShowCheckedModeBanner: false,
 ));
 
 
